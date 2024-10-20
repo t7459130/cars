cf552664bd68949a439649a42cc138a78f2e7b42
t <t7459130@gmail.com>
2024-10-20 22:26:14 +0100

    i

---
+++ src/components/Maintenance.js
@@ -0,0 +1,17 @@
+// src/components/Maintenance.js
+
+import React from 'react';
+import './Maintenance.css'; // Optional: If you have specific styles for this component
+
+const Maintenance = () => {
+  return (
+    <div className="maintenance-overlay">
+      <h1>Down for Maintenance</h1>
+      <p>We will be back with you shortly.</p>
+      <p>Please call us at +44 444 444 4444 for inquiries.</p>
+      <p>Email us at 444@44444.com</p>
+    </div>
+  );
+};
+
+export default Maintenance;
