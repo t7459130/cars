eaa19f320bb0598a6f265b56848b5011b60fd52b
Timo Kiukkanen <timo@kiukkanen.com>
2025-07-31 00:30:20 +0100

    chatgpt needss to be asked twice

--- src/App.js
+++ src/App.js
@@ -14,7 +14,11 @@ import ContactUs from './ContactUs';
 import CarDetail from './CarDetail';
 
 import aboutImage from './images/car1.jpg';
-
+import banner1 from './images/carwallpaper1.jpg';
+import banner2 from './images/carwallpaper2.jpg';
+import banner3 from './images/carwallpaper3.jpg';
+import banner4 from './images/carwallpaper4.jpg';
+import banner5 from './images/carwallpaper5.jpg';
 
 import paganiLogo from './images/pagani.png';
 import mercedesLogo from './images/mercedes.png';
